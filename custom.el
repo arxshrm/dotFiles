346e1ff5550f845b119f655d3328aea2fa871def
arxshrm <arxshrm@gmail.com>
2025-10-15 10:46:55 +0100

    Create custom.el

---
+++ custom.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/arxshrm/life/todo.org"
+     "/home/arxshrm/notes/a2025/cs130/books/discreteMathematicsForComputerScientists.org"
+     "/home/arxshrm/life/UoW/society.org" "/home/arxshrm/notes/a2025/school.org"))
+ '(package-selected-packages '(cdlatex org-download org-roam org-superstar sqlite3)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
